42a9e75308c23d39422e895b26fb9fa497485acb
nawal-ali <nmaaa3899@gmail.com>
2025-02-28 11:48:13 +0200

    generated pages and shared folders for the main structure of the project

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesktopComponent } from './pages/desktop/desktop.component';
+import { LaptopComponent } from './pages/laptop/laptop.component';
+import { TvComponent } from './pages/tv/tv.component';
+import { GamingComponent } from './pages/gaming/gaming.component';
+import { MonitorComponent } from './pages/monitor/monitor.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BluetoothSpeakerComponent } from './pages/bluetooth-speaker/bluetooth-speaker.component';
+import { AccessoriesComponent } from './pages/accessories/accessories.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ProductCardComponent } from './shared/components/product-card/product-card.component';
+import { BannerSliderComponent } from './shared/components/banner-slider/banner-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesktopComponent,
+    LaptopComponent,
+    TvComponent,
+    GamingComponent,
+    MonitorComponent,
+    HomeComponent,
+    BluetoothSpeakerComponent,
+    AccessoriesComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductCardComponent,
+    BannerSliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ project/src/app/shared/components/banner-slider/banner-slider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-slider',
+  standalone: false,
+  templateUrl: './banner-slider.component.html',
+  styleUrl: './banner-slider.component.css'
+})
+export class BannerSliderComponent {
+
+}
