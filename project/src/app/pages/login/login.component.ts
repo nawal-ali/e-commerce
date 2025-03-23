bc47519a4de483c9e64f27343d5e466a51fedd57
nawal-ali <nmaaa3899@gmail.com>
2025-03-23 07:38:04 +0200

    created and testing login component

---
+++ project/src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+  
+    if (this.email && this.password) {
+      if (this.validateEmail(this.email)) {
+        console.log('Login Successful:', this.email);
+        this.router.navigate(['/home']);
+      } else {
+        console.log('Invalid email format');
+      }
+    } else {
+      console.log('Please fill in all fields');
+    }
+  }
+
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+}
