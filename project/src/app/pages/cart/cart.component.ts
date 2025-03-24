0c83a9e049f086a7b08f685e74302ba8d2fdaf25
shazagouda <shazagouda48@gmail.com>
2025-03-24 23:05:01 +0200

    .

--- project/src/app/pages/cart/cart.component.ts
+++ project/src/app/pages/cart/cart.component.ts
@@ -4,8 +4,33 @@ import { Component } from '@angular/core';
   selector: 'app-cart',
   standalone: false,
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  cartItems = [
+    { name: 'Product 1', price: 29.99, quantity: 1, image: '..' },
+    { name: 'Product 2', price: 49.99, quantity: 2, image: 'https://via.placeholder.com/60' },
+    { name: 'Product 3', price: 19.99, quantity: 1, image: 'https://via.placeholder.com/60' },
+    { name: 'Product 4', price: 17.99, quantity: 1, image: 'https://via.placeholder.com/60' }
+  ];
 
+  increaseQuantity(index: number): void {
+    this.cartItems[index].quantity++;
+  }
+
+  decreaseQuantity(index: number): void {
+    if (this.cartItems[index].quantity > 1) {
+      this.cartItems[index].quantity--;
+    }
+  }
+
+  removeItem(index: number): void {
+    this.cartItems.splice(index, 1);
+  }
+  getSubtotal(): number {
+    return this.cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
+  }
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
 }
