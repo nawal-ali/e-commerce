1e284446090abe9f644daed2578cc872f4f5386c
nawal-ali <nmaaa3899@gmail.com>
2025-03-24 23:17:12 +0200

    added the second api

--- project/src/app/shared/services/product.service.ts
+++ project/src/app/shared/services/product.service.ts
@@ -6,7 +6,8 @@ import { map,Observable, BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class ProductService {
-  private apiUrl = 'https://run.mocky.io/v3/2c0ebbbb-f726-4334-9fe8-b6bdf17cb34a';
+  // private apiUrl = 'https://run.mocky.io/v3/2c0ebbbb-f726-4334-9fe8-b6bdf17cb34a';
+  private apiUrl = 'https://run.mocky.io/v3/20454008-1f2f-42d8-941b-61dc145edf8a';
   private selectedCategory = new BehaviorSubject<string>('');
 
   constructor(private http: HttpClient) {}
