6a726011b5831d48df401e250c928a0a5cfe33fc
nawal-ali <nmaaa3899@gmail.com>
2025-05-13 14:31:50 +0300

    added all functions to get category

--- project/src/app/shared/services/product.service.ts
+++ project/src/app/shared/services/product.service.ts
@@ -10,11 +10,46 @@ export class ProductService {
   private selectedCategory = new BehaviorSubject<string>('');
 
   constructor(private http: HttpClient) {}
-
-
   
+  //get all products
   getProducts(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/products/all`); 
+  }
+
+  
+  //get laptop products 
+  getLaptopCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/laptop`)
+  }
+
+  //get dasktop products
+  getDesktopCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/desktop`)
+  }
+
+  //get monitor products
+  getMonitorCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/monitor`)
+  }
+
+  //get tv products
+  getTvCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/tv`)
+  }
+
+  //get gaming products
+  getGamingCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/gaming`)
+  }
+
+  //get accessories products
+  getAccessoriesCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/accessories`)
+  }
+
+  //get bluetooth-speaker products
+  getSpeakerCategory():Observable<any>{
+    return this.http.get(`${this.apiUrl}/products/bluetooth-speaker`)
   }
 
   setSelectedCategory(category: string): void {
@@ -25,6 +60,7 @@ export class ProductService {
     return this.selectedCategory.asObservable();
   }
 
+  // get single product
   getSingleProduct(id: any): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/products/${id}`);
   }
