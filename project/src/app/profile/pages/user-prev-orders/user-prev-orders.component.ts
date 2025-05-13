abe25b20822e8b584d258edfb79f8316175ba60b
nawal-ali <nmaaa3899@gmail.com>
2025-05-13 13:58:36 +0300

    done user prev order page

--- project/src/app/profile/pages/user-prev-orders/user-prev-orders.component.ts
+++ project/src/app/profile/pages/user-prev-orders/user-prev-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../shared/services/user.service';
 
 @Component({
   selector: 'app-user-prev-orders',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './user-prev-orders.component.css'
 })
 export class UserPrevOrdersComponent {
-
+  id = '681e12177c4dff4989d42c20';
+  orders :any;
+  constructor(public userServic: UserService){
+    this.userServic.getUserPrevOrders(this.id).subscribe(res => {
+      if(res){
+        this.orders = res
+        console.log('items from res');
+      }
+    })
+  }
 }
