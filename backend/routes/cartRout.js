577bab17cfa768507c4161d43163b815023b1b3c
nawal-ali <nmaaa3899@gmail.com>
2025-05-13 15:22:22 +0300

    created and added cart model and routes

---
+++ backend/routes/cartRout.js
@@ -0,0 +1,34 @@
+//add to cart
+router.post('/add', async (req, res) => {
+    const { userId, productId, quantity } = req.body;
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+        cart = new Cart({
+            user: userId,
+            items: [{ product: productId, quantity }]
+        });
+    } else {
+        const itemIndex = cart.items.findIndex(item => item.product.toString() === productId);
+        if (itemIndex > -1) {
+            cart.items[itemIndex].quantity += quantity;
+        } else {
+            cart.items.push({ product: productId, quantity });
+        }
+    }
+    await cart.save();
+    res.json(cart);
+});
+
+//get all cart 
+router.get('/:userId', async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ user: req.params.userId }).populate('items.product');
+        if (!cart || cart.items.length === 0) {
+            return res.json({ message: 'Cart is empty' });
+        }
+        res.json(cart);
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
+
--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,14 @@ const productsRouter = require('./routes/products');
 const registerRouter = require('./routes/registerRout');
 const orderRouter = require('./routes/orderRout');
 const loginRouter = require('./routes/loginRout');
+const cartRouter = require('./routes/cartRout');
 
 app.use(cors());
 app.use('/products', productsRouter);
 app.use('/register', registerRouter);
 app.use('/prevOrder', orderRouter);
 app.use('/login', loginRouter);
+app.use('/cart', cartRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
